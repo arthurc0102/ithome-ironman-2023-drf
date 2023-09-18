2be2b62242863ef22dd342fdb6d2a8ad4c7877eb
Arthur Chang <arthurc0102@gmail.com>
2023-09-18 21:30:56 +0800

    feat: add day03

--- server/urls.py
+++ server/urls.py
@@ -3,6 +3,7 @@ URL configuration for server project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
+
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -18,5 +19,5 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
 ]
