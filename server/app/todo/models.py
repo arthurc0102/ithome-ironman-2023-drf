56647eb0cc4d0f8f6ce775311cfaa9fe855cddb0
Arthur Chang <arthurc0102@gmail.com>
2023-09-21 20:13:27 +0800

    feat: add day06

--- server/app/todo/models.py
+++ server/app/todo/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Task(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True)
+    is_finish = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
