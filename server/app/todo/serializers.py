45e8948a12e9203a280ea1202537937dee1abd3a
Arthur Chang <arthurc0102@gmail.com>
2023-09-25 19:28:42 +0800

    feat: add day10

--- server/app/todo/serializers.py
+++ server/app/todo/serializers.py
@@ -3,13 +3,22 @@ from rest_framework import serializers
 from server.app.todo import models as todo_models
 
 
-class TaskSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = todo_models.Task
-        fields = "__all__"
-
-
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = todo_models.Tag
+        fields = "__all__"
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    tags = TagSerializer(many=True, read_only=True)
+    tag_ids = serializers.PrimaryKeyRelatedField(
+        allow_empty=False,
+        write_only=True,
+        many=True,
+        queryset=todo_models.Tag.objects.all(),
+        source="tags",
+    )
+
+    class Meta:
+        model = todo_models.Task
         fields = "__all__"
