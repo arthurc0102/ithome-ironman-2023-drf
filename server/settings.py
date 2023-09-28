4565b8e4b60004daafc53cdc84cf5431dbd4802d
Arthur Chang <arthurc0102@gmail.com>
2023-09-28 18:01:43 +0800

    feat: add day13

--- server/settings.py
+++ server/settings.py
@@ -124,3 +124,15 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# DRF settings
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.BasicAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+}
