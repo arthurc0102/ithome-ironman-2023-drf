1b65aaa932255d3a32c92fcc2678930312247a79
Arthur Chang <arthurc0102@gmail.com>
2023-09-29 23:02:33 +0800

    feat: add day14

--- server/app/todo/admin.py
+++ server/app/todo/admin.py
@@ -1,1 +1,16 @@
-# Register your models here.
+from django.contrib import admin
+
+from server.app.todo import models as todo_models
+
+
+@admin.register(todo_models.Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("title", "is_finish")
+    list_filter = ("is_finish", "tags")
+    search_fields = ("title", "description")
+    filter_horizontal = ("tags",)
+
+
+@admin.register(todo_models.Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
