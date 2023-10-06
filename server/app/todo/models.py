da60cc3ad0c6aaf4024d0e5cd5d87aca2e29ec12
Arthur Chang <arthurc0102@gmail.com>
2023-10-06 17:14:26 +0800

    feat: add day21

--- server/app/todo/models.py
+++ server/app/todo/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Tag(models.Model):
     name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True)
 
     def __str__(self):
         return self.name
@@ -13,6 +14,9 @@ class Task(models.Model):
     description = models.TextField(blank=True)
     is_finish = models.BooleanField(default=False)
     tags = models.ManyToManyField(Tag)
+    end_at = models.DateTimeField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
