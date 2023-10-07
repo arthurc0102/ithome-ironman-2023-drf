564424682a3dbdc7f826d6096a31e107f745533c
Arthur Chang <arthurc0102@gmail.com>
2023-10-07 20:56:48 +0800

    feat: add day22

--- server/app/todo/models.py
+++ server/app/todo/models.py
@@ -9,6 +9,13 @@ class Tag(models.Model):
         return self.name
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Task(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True)
@@ -17,6 +24,7 @@ class Task(models.Model):
     end_at = models.DateTimeField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.title
